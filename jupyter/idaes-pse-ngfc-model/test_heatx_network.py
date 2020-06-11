278624d536a09adc3190439b3b02e9df83d9c7d2
Henry Lee <hjlee108@uw.edu>
2020-06-11 01:07:45 -0700

    To test modules made

--- jupyter/idaes-pse-ngfc-model/test_heatx_network.py
+++
@@ -1,9 +0,0 @@
-import heatx_network
-from pyomo.environ import ConcreteModel
-
-
-def declare_heatx_unit():
-    h = ConcreteModel()
-    assert type(heatx_network.declare_heatx_unit(h)) == ConcreteModel, \
-        "Warning: Incorrect type"
-    return
